8f70a439bdfa17e79a39d590709ff9d24fba10ad
Jongmin Lee <59940249+ljm0223@users.noreply.github.com>
2020-02-14 15:11:46 -0600

    Final project of Lab4-postage2
    codes that calculate cost of postage by using flowchart 2

---
+++ Flowchart2.java
@@ -0,0 +1,22 @@
+package postage2;
+public class Flowchart2 {
+	
+		public double weight;
+		public double cost;
+		public double flowchart2()
+		{
+		if (weight <= 1){
+	          cost = 0.47;}
+			if (weight > 1) {
+				cost = 0.47+ Math.ceil(weight-1) * 0.21;
+			}
+			if (weight > 3.5) {
+				cost = 0.94 + Math.ceil(weight - 1) * 0.21;
+			}
+	            else
+	            { cost = 0.47 + Math.ceil(weight - 1) * 0.21;}
+		return cost;
+		}
+
+	}
+
