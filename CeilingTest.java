f6e2f060811b7a2601951e71f9941e65b63e43e7
Jongmin Lee <59940249+ljm0223@users.noreply.github.com>
2020-02-14 14:45:53 -0600

    Second checkpoint of Lab4
    Learned how to use a debugger and fixed errors at Donut.java and fixed errors at piggyBank.java use piggyBankExample.java

--- CeilingTest.java
+++ CeilingTest.java
@@ -1,15 +1,12 @@
-package labPractice;
+package lab4;
 
-public class CeilingTest {
-
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-		System.out.println(Math.ceil(1.3));
-		System.out.println(Math.ceil(2.0));
-		System.out.println(Math.ceil(2.1));
-		System.out.println(Math.ceil(-2.1));
-	}
-
+public class CeilingTest
+{
+  public static void main(String[] args)
+  {
+    System.out.println(Math.ceil(1.3));
+    System.out.println(Math.ceil(2.0));
+    System.out.println(Math.ceil(2.1));
+    System.out.println(Math.ceil(-2.1));
+  }
 }
- 
- 
