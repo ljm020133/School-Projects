c27ae5a3069234cc066af7a5800e239d657ce80b
Jongmin Lee <59940249+ljm0223@users.noreply.github.com>
2020-02-14 13:02:30 -0600

    Add files via upload

---
+++ DifferentWaysToPrint.java
@@ -0,0 +1,21 @@
+package jan15;
+
+public class DifferentWaysToPrint {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+System.out.println("Hello World");
+System.out.println("Hello World");
+System.out.print("Hello");
+System.out.println("World");
+System.out.print("Hey");
+String str = "abc";
+int num = 5;
+char ch = 'a';
+System.out.print(str);
+		}
+
+
+}
+
+
