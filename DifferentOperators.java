0d3c99d913e567ef1eda85233b86c3af7d0c9d77
Jongmin Lee <59940249+ljm0223@users.noreply.github.com>
2020-02-14 13:07:17 -0600

    Add files via upload

---
+++ DifferentOperators.java
@@ -0,0 +1,26 @@
+
+public class DifferentOperators {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+int a = 10;
+int b = 35;
+int c = a+b;
+System.out.print("answer of a+b is ");
+System.out.println(a+b);
+// "+" is add
+// "-" is subtract
+// "*" is multiply
+// "/" is divide
+// "%" is give remainders from divide
+// when you check two are equal you use double "=" sign
+//ex) "a == b"
+// "!" is used as meaning "not"
+// "!=" means not equal
+//ex) "!(a==b)" this check a is not equal with b
+System.out.println(a==b);
+//System.out.println(a=b);
+System.out.println(!(a==b));
+	}
+
+}
