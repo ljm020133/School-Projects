49ab13c605941f5fa371ab347dcd4b672ad5e447
Jongmin Lee <59940249+ljm0223@users.noreply.github.com>
2020-02-14 13:13:48 -0600

    Add files via upload

---
+++ quizReview.java
@@ -0,0 +1,15 @@
+package coms227;
+import java.util.Scanner;
+
+public class quizReview {
+	public static void main(String [] args) {
+		Scanner scan = new Scanner(System.in);
+		int i = scan.nextInt();
+		char c =scan.next().charAt(0);
+		String s = scan.nextLine();
+		System.out.println("Enter number" + i);
+		
+		
+	}
+
+}
